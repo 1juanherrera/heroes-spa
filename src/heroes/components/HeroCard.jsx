6e5a987125430a51df679cca3e4c71e0fdd01959
1juanherrera <juansebas751@gmail.com>
2023-06-08 23:28:17 -0500

    add: HeroCard and HeroList

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -9,11 +9,34 @@ const HeroCard = ({
     characters
 }) => {
 
+    const heroImageUrl = `/assets/heroes/${id}.jpg`
 
     return ( 
-        <>
-            <h3>{ superhero }</h3>
-        </>
+        <div className="col">
+            <div className="card">
+                <div className="row no-gutters">
+                    <div className="col-4">
+                        <img src={ heroImageUrl } className="card-img" alt={ superhero }/>
+                    </div>
+
+                    <div className="col-8">
+                        <div className="card-body">
+                            <h5 className="card-title text-base">{ superhero }</h5>
+                            <p className="card-text">{ alter_ego }</p>
+
+                            {
+                                alter_ego !== characters ? <p>{ characters }</p> : ''
+                            }
+
+                            <p className="card-text">
+                                <small className="text-secondary">{ first_appearance }</small>
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
