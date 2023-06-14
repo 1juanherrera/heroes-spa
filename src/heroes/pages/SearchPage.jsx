dca753cb0cbb9b40e39ea8ef411748041683a818
1juanherrera <juansebas751@gmail.com>
2023-06-14 14:59:25 -0500

    add: SearchPage

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,4 +1,4 @@
-
+import HeroCard from '../components/HeroCard'
 
 const SearchPage = () => {
 
@@ -6,7 +6,45 @@ const SearchPage = () => {
     return (
         <>
             <h1>Search</h1>
-            <input type="text" />
+            <hr />
+
+            <div className="row">
+
+            <div className="col-5">
+                <h4>Searching</h4>
+                <hr />
+                <form>
+                    <input 
+                    placeholder="Search a hero"
+                    className="form-control"
+                    autoComplete="off"
+                    name="searchText"
+                    type="text"/>
+
+                    <button className="btn btn-outline-dark mt-3">
+                        Search
+                    </button>
+                </form>
+            </div>
+
+            <div className="col-7">
+                <h4>Results</h4>
+                <hr />
+
+                <div className="alert alert-dark">
+                    Search a hero
+                </div>
+
+                <div className="alert alert-danger">
+                    No hero with <b>ABC</b>
+                </div>
+
+                {/* <HeroCard { ...hero } /> */}
+
+            </div>
+
+            </div>
+
         </>
     )
 }
